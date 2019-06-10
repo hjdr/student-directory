b784d23c144c47bf0ef8c21f390016c1a8d1cc09
hjdr <harry.jdr@gmail.com>
2019-06-10 13:36:24 +0100

    added user input functionaility

--- dictionary.rb
+++ dictionary.rb
@@ -1,32 +1,32 @@
 # list of students at Villains Academy
 
-students =[
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def get_name
+  students = []
+  while true
+    puts "Enter student name, or enter 'exit' to quit"
+    student_name = gets.chomp
+    break if student_name == "exit"
+    students << {name: student_name, cohort: :November }
+    print_footer(students)
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(student_info)
-  student_info.each { |student| puts " student: #{student[:name]} (#{student[:cohort]} cohort)" }
+def print_student_info(student_info)
+  student_info.each { |student| puts "student: #{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer(student_info)
   puts "Overall, we have #{student_info.count} great students"
 end
 
+students = get_name
+puts ""
 print_header
-print(students)
+print_student_info(students)
 print_footer(students)
